d6b6592eac5726688cc6da8421f255904225e0d4
Virendra Kumar <virendrak790@gmail.com>
2017-10-15 18:34:56 +0530

    Update MusicController.java

--- src/com/player/androidmusicplayer/MusicController.java
+++ src/com/player/androidmusicplayer/MusicController.java
@@ -3,12 +3,6 @@ package com.player.androidmusicplayer;
 import android.content.Context;
 import android.widget.MediaController;
 
-/*
- * This is demo code to accompany the Mobiletuts+ series:
- * Android SDK: Creating a Music Player
- * 
- * Sue Smith - February 2014
- */
 
 public class MusicController extends MediaController {
 
